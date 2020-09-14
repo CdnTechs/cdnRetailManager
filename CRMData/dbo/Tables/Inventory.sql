b6e07be66b97c29b2f89eb69b70812b3630fd938
Chandan Kumar <chandan.k@ehealthflex.com>
2020-09-14 23:38:43 +0530

    Register DB table
    The table for the basic register system

---
+++ CRMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1, 
+    [PurchagePrice] MONEY NOT NULL, 
+    [PurchageDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
